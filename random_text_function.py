3ed9f4792d9a5e85ad586bf23c900696b0b78a55
kvytautas <7vytautas7@gmail.com>
2026-02-24 13:57:56 +0200

    #2 Sunkesni 20260224

--- random_text_function.py
+++ random_text_function.py
@@ -6,3 +6,9 @@ def generate_rnd_str(length):
   for i in range(length):
     text += symbols[random.randint(0,len(symbols) -1)]
   return text
+
+def rnd_num_array(min, max, length):
+  array = []
+  for i in range(length):
+    array.append(random.randint(min, max))
+  return array
--- sunkesni_funkcijos.py
+++ sunkesni_funkcijos.py
@@ -1,3 +1,5 @@
+import random
+
 # 1.	Parašykite funkciją, kurios argumentas būtų tekstas, kuris būtų atspausdinamas konsolėje pridedant “---” pradžioje ir gale. PVZ (---labas---)
 print("--------1--------")
 from random_text_function import generate_rnd_str
@@ -11,26 +13,44 @@ def hard_one(txt):
 hard_one(rnd_text)
 
 # 2.	Sugeneruokite atsitiktinį stringą iš raidžių ir skaičių (10 simbolių). Atspausdinkite simbolius stulpeliu. Jei tai skaičius apgaubkite “ [ 7 ]”. Jei skaičiai eina keli iš eilės, apgaubkite juos kartu. [75].
-print("--------2--------")
+# print("--------2--------")
+#
+# def print_symbols(txt):
+#     number_check = ""   # skaiciu eile
+#
+#     for i in txt:
+#         if i.isdigit():
+#             number_check += i
+#         else:
+#             if number_check != "":
+#                 print(f"[{number_check}]")
+#                 number_check = ""
+#             print(i)
+#     if number_check != "":
+#         print(f"[{number_check}]")
+#
+# print_symbols(rnd_text)
 
-def print_symbols(txt):
-    number_buffer = ""   # stores consecutive digits
+# 3.	 Parašykite funkciją, kuri skaičiuotų, ir gražintų iš kiek sveikų skaičių jos argumentas dalijasi be liekanos (išskyrus vienetą ir patį save). Pvz padavus 10 turi grąžinti 2,  o padavus 20 gražintų 4.
+print("--------3--------")
 
-    for ch in txt:
-        if ch.isdigit():
-            # build a row of numbers
-            number_buffer += ch
-        else:
-            # if we have collected numbers, print them first
-            if number_buffer != "":
-                print(f"[{number_buffer}]")
-                number_buffer = ""
+# rnd_num = random.randint(1,1000)
+# print(rnd_num)
 
-            # print the non-number symbol
-            print(ch)
+def divisor_count(number):
+    counter = 0
+    # if number > 1 and all(number % i != 0 for i in range(2, int(number**0.5) + 1)):
+    #     return 0
+    for i in range(2, int(number**0.5) + 1):
+            if number % i == 0:
+                counter += 1
+                if i != number // i:
+                    counter += 1
 
-    # print leftover numbers at the end
-    if number_buffer != "":
-        print(f"[{number_buffer}]")
+    return counter
 
-print_symbols(rnd_text)
+print(divisor_count(20))
+
+# 4.	Sugeneruokite masyvą iš 100 elementų, kurio reikšmės atsitiktiniai skaičiai nuo 33 iki 77. Išrūšiuokite masyvą pagal daliklių be liekanos kiekį mažėjimo tvarka, panaudodami trečio uždavinio funkciją.
+print("--------4--------")
+
